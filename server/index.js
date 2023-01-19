502c86e5dcd68ccb036c50a60c536395e858c1a2
Matthew Brignola <matthewbrignola@ymail.com>
2023-01-18 21:45:32 -0700

    used escape character to prevent sql injection and now user can log in on abck end. need to connect to form in front end

--- server/index.js
+++ server/index.js
@@ -1,26 +1,39 @@
-const env = require("dotenv").config();
+const dotenv = require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const mysql = require("mysql2");
 const PORT = 8080;
-const app = express();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
 });
 
-const getUsers = "select * from users";
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+connection.connect(() => console.log("connected to db"));
 
 app.get("/", (req, res) => {
-  connection.query(getUsers, (err, results) => {
-    if (err) throw err;
-    results;
-    res.send(results);
-  });
+  res.json("helloworld");
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  connection.query(
+    `select * from users  where email=? and password=?`,
+    [email, password],
+    (err, results) => {
+      if (err) throw err;
+      res.send(results[0].email + " successfully logged in.");
+    }
+  );
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`App listening at ${PORT}`);
 });
