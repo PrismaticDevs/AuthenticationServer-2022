36e6289ea19d6fbfa1ac4ad4f3cce46f43b7fe23
Matthew Brignola <matthewbrignola@ymail.com>
2023-01-29 12:25:26 -0700

    updated auth form response messages and css

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -53,94 +53,102 @@ const Auth = () => {
   return (
     <>
       <div className="form-container">
-        <form autoComplete="off" onSubmit={submitLoginForm}>
-          <div className="App-header">
-            <h1>Login</h1>
-          </div>
-          <label>Email</label>
-          <input
-            type="text"
-            required
-            placeholder="Email"
-            autoComplete="off"
-            onChange={(e) => setLoginEmail(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                submitLoginForm();
-              }
-            }}
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            required
-            placeholder="Password"
-            autoComplete="off"
-            onChange={(e) => setLoginPassword(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                submitLoginForm();
-              }
-            }}
-          />
-          <input type="button" value="Login" onClick={submitLoginForm} />
+        <div className="subForm">
+          <form autoComplete="off" onSubmit={submitLoginForm}>
+            <div className="App-header">
+              <h1>Login</h1>
+            </div>
+            <label>Email</label>
+            <input
+              type="text"
+              required
+              placeholder="Email"
+              autoComplete="off"
+              onChange={(e) => setLoginEmail(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  submitLoginForm();
+                }
+              }}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              required
+              placeholder="Password"
+              autoComplete="off"
+              onChange={(e) => setLoginPassword(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  submitLoginForm();
+                }
+              }}
+            />
+            <input type="button" value="Login" onClick={submitLoginForm} />
+          </form>
           <p
             className={status ? "good" : "bad"}
             style={{ fontSize: "1.5em", textAlign: "center" }}
           >
             {loginMessage}
           </p>
-        </form>
-        <form autoComplete="off" onSubmit={submitRegistrationForm}>
-          <div className="App-header">
-            <h1>Register</h1>
-          </div>
-          <label>Username</label>
-          <input
-            type="text"
-            required
-            placeholder="Username"
-            onChange={(e) => setUsername(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                submitRegistrationForm();
-              }
-            }}
-          />
-          <label>Email</label>
-          <input
-            type="text"
-            required
-            placeholder="Email"
-            autoComplete="off"
-            onChange={(e) => setRegistrationEmail(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                submitRegistrationForm();
-              }
-            }}
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            required
-            placeholder="Password"
-            autoComplete="off"
-            onChange={(e) => setRegistrationPassword(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                submitRegistrationForm();
-              }
-            }}
-          />
-          <input type="button" value="Login" onClick={submitRegistrationForm} />
+        </div>
+        <div className="subForm">
+          <form autoComplete="off" onSubmit={submitRegistrationForm}>
+            <div className="App-header">
+              <h1>Register</h1>
+            </div>
+            <label>Username</label>
+            <input
+              type="text"
+              required
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  submitRegistrationForm();
+                }
+              }}
+            />
+            <label>Email</label>
+            <input
+              type="text"
+              required
+              placeholder="Email"
+              autoComplete="off"
+              onChange={(e) => setRegistrationEmail(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  submitRegistrationForm();
+                }
+              }}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              required
+              placeholder="Password"
+              autoComplete="off"
+              onChange={(e) => setRegistrationPassword(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  submitRegistrationForm();
+                }
+              }}
+            />
+            <input
+              type="button"
+              value="Login"
+              onClick={submitRegistrationForm}
+            />
+          </form>
           <p
             className={status ? "good" : "bad"}
             style={{ fontSize: "1.5em", textAlign: "center" }}
           >
             {registrationMessage}
           </p>
-        </form>
+        </div>
       </div>
     </>
   );
