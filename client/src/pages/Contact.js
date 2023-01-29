6815ac64e879931923735ff0a5218daea66dad43
Matthew Brignola <matthewbrignola@ymail.com>
2023-01-28 19:34:05 -0700

    added registration and srticle css

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,19 +1,39 @@
 import React from "react";
+import Article from "../components/Article";
+import "../App.css";
 
 const Contact = () => {
+  const body = (
+    <>
+      <h1>Hey</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut consectetur
+        excepturi soluta nulla officia corporis. Minima obcaecati suscipit
+        porro. Id, repellat reprehenderit? Optio recusandae obcaecati
+        repellendus quis, aut expedita suscipit magnam laudantium commodi magni
+        dignissimos aspernatur ea dolores illo placeat, fugit numquam quod
+        corrupti dolor deserunt neque quos minus eaque.
+      </p>
+      <h3>Hoopla</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem amet
+        optio reprehenderit velit ab mollitia commodi consequatur qui rem
+        architecto, incidunt excepturi temporibus eius sed molestias. Rerum
+        minima et impedit tenetur nihil vitae quia sunt!
+      </p>
+    </>
+  );
   return (
     <>
       <div className="App-header">
         <h1>Contact</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia illo
-          repellat nesciunt consequuntur! Hic nulla autem vitae dolorem quas!
-          Quis veritatis debitis eos, animi sed facilis iste voluptas autem
-          consectetur sunt, corporis, sint laboriosam voluptatibus amet repellat
-          ea blanditiis ratione obcaecati atque. Nostrum mollitia cupiditate
-          assumenda aliquam modi iste laudantium!\
-        </p>
       </div>
+      <Article
+        title="Blah"
+        body={body}
+        author="Matt Brignola"
+        date="1/28/2022"
+      />
     </>
   );
 };
