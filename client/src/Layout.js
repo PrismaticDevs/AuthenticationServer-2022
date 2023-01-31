479e2c1cb4fe96e39b6d7964281ff92dea247c0e
Matthew Brignola <matthewbrignola@ymail.com>
2023-01-30 20:53:11 -0700

    front major changes to sidebar implemented now in nav

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import LogoutIcon from "@mui/icons-material/Logout";
 import LoginIcon from "@mui/icons-material/Login";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import Modal from "./components/Modal";
 import Sidebar from "./components/Sidebar";
 
@@ -27,34 +29,46 @@ const Layout = () => {
   useEffect(() => {
     getToken();
   }, []);
+  const [showHTML, setShowHTML] = useState(false);
+  const toggleHTML = () => setShowHTML(!showHTML);
   return (
     <>
-      <div>
+      <div className="sidebar">
         <nav>
           <ul>
             <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Contact</NavLink>
+              <button
+                className={showHTML ? "show" : "hide"}
+                onClick={toggleHTML}
+              >
+                HTML & CSS
+                {showHTML ? <ChevronRightIcon /> : <KeyboardArrowDownIcon />}
+              </button>
+              {showHTML ? (
+                <>
+                  <li>
+                    <NavLink to="/">HTML & CSS</NavLink>
+                  </li>
+                </>
+              ) : (
+                <></>
+              )}
             </li>
             {token.name ? (
-              <li className="logout">
+              <>
                 <button className="logout logout-btn" onClick={logout}>
                   <span title="Logout">
                     <LogoutIcon />
                   </span>
                 </button>
                 <p className="logout text">User: {token.name}</p>
-              </li>
+              </>
             ) : (
-              <li className="login">
-                <NavLink className="login" to="/auth">
-                  <span title="Login">
-                    <LoginIcon className="login" />
-                  </span>
-                </NavLink>
-              </li>
+              <NavLink className="login" to="/auth">
+                <span title="Login">
+                  <LoginIcon className="login" />
+                </span>
+              </NavLink>
             )}
           </ul>
           <Sidebar />
---
+++ client/src/pages/Basics.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./css/basics.css";
+import "../App.css";
+
+const Basics = () => {
+  return (
+    <div className="page">
+      <h1>The Basics</h1>
+      <ul>
+        <li>Understand the DOM</li>
+        <li>Structure a webpage (HTML)</li>
+        <li>Style the web page (CSS)</li>
+        <li>Make it interactive (JavaSCript)</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Basics;
