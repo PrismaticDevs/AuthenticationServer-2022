95d2cbd6e3a3fc3dd6547ebd1f1e2f561ff4bed5
Matthew Brignola <matthewbrignola@ymail.com>
2023-02-05 16:58:32 -0700

    code card

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./css/card.css";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { Tooltip } from "@mui/material";
 
 const Card = (props) => {
   return (
     <>
       <div className="card">
-        <p className="code">{props.code}</p>
+        <p className="code">
+          {props.code}
+          <Tooltip title="Copy code">
+            <ContentCopyIcon className="copy" />
+          </Tooltip>
+        </p>
       </div>
     </>
   );
--- client/src/pages/Project1.js
+++ client/src/pages/Project1.js
@@ -7,6 +7,13 @@ const Project1 = () => {
       <h2>HTML</h2>
       <p>&lt;p&gt; Hello World! &lt;/p&gt;</p>
       <h1>&lt;div&gt; Hello World! &lt;/div&gt;</h1>
+      <p>{`const dogs = ['corgis', 'dobermans', 'huskies']`}</p>
+      <p>{`for (let i = 0; i &lt; dogs.length; i++;) {`}</p>
+      <p>{`{ "I love " dogs[i]}`}</p>
+      <p>Output:</p>
+      <p>{`I love Corgis`}</p>
+      <p>{`I love dobermans`}</p>
+      <p>{`I love huskies`}</p>
     </>
   );
   return (
